9244035ab083a59f90608be24935a326f0fd32b3
sullaspqr <sulla.spqr78@gmail.com>
2024-11-10 12:20:56 +0100

    create-script.js változás - másolja át a package.json-t is!

--- create-script.js
+++ create-script.js
@@ -5,7 +5,9 @@ const path = require('path');
 // A sablonfájlok könyvtárának megadása
 const srcTemplateDir = path.join(__dirname, '/template/src');
 const publicTemplateDir = path.join(__dirname, '/template/public');
-const destDir = process.cwd();
+const srcPackageJson = path.join(__dirname, 'package.json');  // A főkönyvtárban lévő package.json
+const destDir = process.cwd();  // A célkönyvtár (ahová a fájlokat másoljuk)
+
 // Függvény a másolás végrehajtására és hiba kezelésére
 const copyDirectory = async (srcDir, destDir) => {
   try {
@@ -16,8 +18,19 @@ const copyDirectory = async (srcDir, destDir) => {
   }
 };
 
+// Függvény a package.json másolására
+const copyPackageJson = async (srcFile, destDir) => {
+  try {
+    await fs.copy(srcFile, path.join(destDir, 'package.json'));
+    console.log('Successfully copied package.json to project root!');
+  } catch (err) {
+    console.error('Error copying package.json:', err);
+  }
+};
+
 // Másolások végrehajtása
 (async () => {
   await copyDirectory(srcTemplateDir, destDir);
   await copyDirectory(publicTemplateDir, destDir);
+  await copyPackageJson(srcPackageJson, destDir);  // A package.json fájl másolása
 })();
